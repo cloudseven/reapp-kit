80867055b4d268cf25ee6ee81ae92a3547350158
Timur Carpeev <timuric@users.noreply.github.com>
2015-10-27 12:58:01 +0100

    Remove store

--- src/lib/ContextTypes.js
+++ src/lib/ContextTypes.js
@@ -3,7 +3,6 @@ var React = require('react');
 module.exports = {
   theme: React.PropTypes.object,
   animations: React.PropTypes.object,
-  store: React.PropTypes.func,
   action: React.PropTypes.func,
   router: React.PropTypes.func
 };
