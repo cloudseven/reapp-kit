4c8df55419983f7d9204ff5ed0d5682babca3135
Timur Carpeev <timuric@users.noreply.github.com>
2015-10-27 12:57:42 +0100

    Remove store

--- src/lib/Reapp.js
+++ src/lib/Reapp.js
@@ -1,7 +1,6 @@
 var React = require('react');
 var RoutedViewListMixin = require('reapp-routes/react-router/RoutedViewListMixin');
 var Components = require('reapp-ui/all');
-var store = require('./store');
 var theme = require('./theme');
 var action = require('./action');
 var ContextTypes = require('./ContextTypes');
@@ -24,18 +23,12 @@ module.exports = function(opts, Component) {
       getChildContext: function() {
         return {
           theme: theme(),
-          store: store.cursor(),
           action: action
         };
       },
 
       componentWillMount() {
         this.forceUpdater = () => this.forceUpdate();
-        store.cursor() && store.cursor().listen(this.forceUpdater);
-      },
-
-      componentWillUnmount() {
-        store.cursor() && store.cursor().unlisten(this.forceUpdater);
       },
 
       render: function() {
